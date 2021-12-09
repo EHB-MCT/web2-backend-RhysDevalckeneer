cc235a64f2f677a3b7983f3a0658e207e52e3f68
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-09 12:41:36 +0100

    Added post

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, u
 const app = express();
 const port = process.env.PORT || 1337;
 
-app.use(cors())
 app.use(express.static('public'));
 app.use(bodyParser.json());
+// "Pyp, open the f*cking gate!" - https://www.youtube.com/watch?v=u5sRJqpRpsU
+app.use(cors());
+
 
 // basic route
 app.get('/', (req, res) => {
@@ -76,6 +78,102 @@ app.get('/movie/:id', async (req,res) => {
     }
 });
 
+// save a challenge
+app.post('/movie', async (req, res) => {
+
+    if(!req.body.movie_id || !req.body.poster_path){
+        res.status(400).send('Bad request: missing name, course or points');
+        return;
+    }
+
+    try{
+         //connect to the db
+        await client.connect();
+
+         //retrieve the challenges collection data
+        const collection = client.db('courseproject').collection('movies');
+
+         // Validation for double challenges
+        const movie = await collection.findOne({name: req.body.name });
+        if(movie){
+            res.status(400).send(`Bad request: Challenge already exists with name ${req.body.name} for course ${req.body.course}` );
+            return;
+        } 
+         // Create the new Challenge object
+        let newMovie = {
+            name: req.body.name,
+            movie_id: req.body.movie_id,
+            backdrop_path: req.body.backdrop_path,
+            genres: req.body.genres,
+            imdb_id: req.body.imdb_id,
+            original_language: req.body.original_language,
+            original_title: req.body.original_title,
+            overview: req.body.overview,
+            poster_path: req.body.poster_path,
+            release_date: req.body.release_date,
+            revenue: req.body.revenue,
+            runtime: req.body.runtime,
+            tagline: req.body.tagline,
+            title: req.body.title,
+            vote_average: req.body.vote_average,
+            vote_count: req.body.vote_count,
+        }
+        // Add the optional session field
+        if(req.body.session){
+            newMovie.session = req.body.session;
+        }
+        
+         // Insert into the database
+        let insertResult = await collection.insertOne(newMovie);
+
+         //Send back successmessage
+        res.status(201).json(newMovie);
+        return;
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    }finally {
+        await client.close();
+    }
+});
+
+// delete moves
+app.delete('/movie/:id', async (req,res) => {
+    
+    if(!req.params.id){
+        res.status(400).send({
+            error: 'Bad Request',
+            value: 'No id available in url'
+        });
+        return;
+    }
+
+    try{
+         //connect to the db
+        await client.connect();
+
+         //retrieve the challenges collection data
+        const collection = client.db('courseproject').collection('movies');
+
+         // Validation for double challenges
+        let movie = await collection.deleteOne({_id: ObjectId(req.params.id)});
+         
+        //Send back successmessage
+        res.status(201).json(movie);
+        return;
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    }finally {
+        await client.close();
+    }
+});
 
 
 
