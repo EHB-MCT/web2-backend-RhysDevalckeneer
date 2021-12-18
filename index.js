2299cc1f0b12a6de82e6da4e3998e27edb7d187e
Rhys Devalckeneer <rhys.devalckeneer@student.ehb.be>
2021-12-18 14:06:54 +0100

    added poster_path

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post('/movies', async (req, res) => {
          // Create the new Challenge object
         let newMovie = {
             movie_id: req.body.movie_id,
-            poster_path: "some path",
+            poster_path: req.body.poster_path,
         }
 
          // Insert into the database
